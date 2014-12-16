6c18a8fc8d82e3f03e7fa2201c15ee0f45999290
Duncan-ASL <duncan_khosla@asl.org>
2014-12-16 15:07:04 +0000

    Update TestChallenge2.java

--- src/TestChallenge2.java
+++ src/TestChallenge2.java
@@ -1,3 +1,6 @@
+
+import edu.fcps.karel2.Display;
+
 /**
  * Read and implement all of the TODO items in the main method, below.
  * 
@@ -7,9 +10,15 @@ public class TestChallenge2 {
 
 	public static void main(String[] args) {
 		// TODO Load the "pasture" map at size 15 by 15
+		Display.openWorld("src/pasture.map");
+        Display.setSize(15, 15);
+        Display.setSpeed(3);
 		// TODO Create an instance of a Horse inside the pasture
+        Horse rex = new Horse(5,3);
 		// TODO Have the horse wander for 36 steps with a timer of 7
+        rex.wander(36, 7);
 		// TODO Have the horse explode()
+        rex.explode();
 	}
 
 }
