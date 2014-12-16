14481fd88ba28ce6584a09aef578fdc8baa6b8bd
Duncan-ASL <duncan_khosla@asl.org>
2014-12-16 15:05:44 +0000

    Update Sentry.java

--- src/Sentry.java
+++ src/Sentry.java
@@ -1,4 +1,8 @@
+
+
 import org.asl.karelx.Uberbot;
+
+import edu.fcps.karel2.Display;
 
 /**
  * Patrols an area defined by four beepers.
@@ -6,15 +10,28 @@ import org.asl.karelx.Uberbot;
  */
 public class Sentry extends Uberbot {
 
+	public Sentry(int x, int y, int dir, int beep) {
+		super(x, y, dir, beep);
+	}
+
 	// Provide one or more constructors, as you wish
-	
+
 	/**
-	 * Patrol an area in a clockwise direction.  
+	 * Patrol an area in a clockwise direction.
 	 * 
-	 * Move forward until next to a beeper then turn right.  Repeat indefinitely.
+	 * Move forward until next to a beeper then turn right. Repeat indefinitely.
 	 *
 	 */
 	public void patrol() {
-		// TODO You implement this method.
+		move();
+		while (true) 
+		{
+			while (!nextToABeeper()) 
+			{
+				move();
+			}
+			turnLeft();
+			move();
+		}
 	}
 }
